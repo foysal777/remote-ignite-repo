927f8db8f42801b0a59463d3297317e3e658de67
Foysal Munna <foysal.cse13@gmail.com>
2026-01-13 15:49:58 +0600

    add timelimit

--- project_root/settings.py
+++ project_root/settings.py
@@ -79,6 +79,21 @@ REST_FRAMEWORK = {
     ),
 }
 
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),     
+    "ROTATE_REFRESH_TOKENS": True,                 
+    "BLACKLIST_AFTER_ROTATION": True,                
+    
+ 
+    "ALGORITHM": "HS256",
+    "SIGNING_KEY": SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+
+}
+
 # --------------------------------------------------
 # MIDDLEWARE
 # --------------------------------------------------
