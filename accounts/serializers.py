18da5434d66bc1fdbce2ba3cc918faa023308c92
Foysal Munna <foysal.cse13@gmail.com>
2026-02-23 09:55:33 +0600

    serializers add

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -113,3 +113,76 @@ class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "email", "register_date", "role", "plan_type"]
+
+
+
+
+
+# add + delete ----------------------  user --------------
+# accounts/serializers.py
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "password",
+            "role",
+            "plan_type",
+            "is_plan_paid",
+            "plan_start_date",
+            "plan_end_date",
+            "total_time",
+            "extra_prompts",
+            "is_active",
+            "is_staff",
+        ]
+        extra_kwargs = {
+            "is_staff": {"read_only": True},
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        
+        user = User.objects.create_user(password=password, **validated_data)
+        return user
+
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False, min_length=6)
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
+            "password",
+            "role",
+            "plan_type",
+            "is_plan_paid",
+            "plan_start_date",
+            "plan_end_date",
+            "total_time",
+            "extra_prompts",
+            "is_active",
+        ]
+        read_only_fields = ["email"]  
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if password:
+            instance.set_password(password)
+
+        instance.save()
+        return instance
